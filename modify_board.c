649bb5a147c825ed3fc8adf55b6369b7d8d73fd0
AlexLaleu <alexandre.laleu@ensta-paris.fr>
2024-04-09 17:27:23 +0200

    fonction modifiant le plateau de jeu

---
+++ modify_board.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "modify_board.h"
+#include "get.h"
+
+void modify_board(int*** board, int piece, int column_index, int row_index){
+    int i=get_row_pos(board,piece,size);
+    int j=get_column_pos(board,piece,size);
+    (*board)[i][j]=0;
+    (*board)[row_index][column_index] = piece;
+}
