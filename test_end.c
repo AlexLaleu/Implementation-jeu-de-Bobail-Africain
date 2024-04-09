f5e3be262951fff11b3892c861d82b6d8a9f3144
AlexLaleu <alexandre.laleu@ensta-paris.fr>
2024-04-09 16:37:52 +0200

    fonction pour tester si la fin du jeu est atteinte

---
+++ test_end.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "test_end.h"
+#include "get.h"
+
+int test_end(int** board,int size,int current_player){ //doit renvoyer 0 si le jeu continue, le numéro du joueur gagnant sinon
+    
+    int i=get_row_pos(board,-1,size);
+    int j=get_column_pos(board,-1,size);
+    if(i==0){return 1;}
+    if(i==4){return 2;}
+    if(j==0){
+        if( (board[i-1][j]!=0) && (board[i+1][j]!=0) && (board[i-1][j+1]!=0) && (board[i][j+1]!=0) && (board[i+1][j+1]!=0) ) {return current_player;}
+    }
+    if(j==4){
+        if( (board[i-1][j]!=0) && (board[i+1][j]!=0) && (board[i-1][j-1]!=0) && (board[i][j-1]!=0) && (board[i+1][j-1]!=0) ) {return current_player;}
+    }
+    else{
+        return 0;
+    }
+}
