38a59d6493536992260e9ee5a3d49ed6869bdde4
desiaas <martinmateo458@gmail.com>
2024-04-23 15:47:03 +0200

    ajout du fichier is_legal

---
+++ is_legal.h
@@ -0,0 +1,7 @@
+#ifndef IS_LEGAL_h
+#define IS_LEGAL_H
+
+bool is_legal(int** board,int piece, int row_pos, int column_pos, int column_index, int row_index)
+    // doit renvoyer true si le coup est légal, false sinon. Attention faire disjonction de cas si la piece est le pion jaune ou non
+
+#endif
